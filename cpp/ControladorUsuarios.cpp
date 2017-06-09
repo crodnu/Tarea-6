e4991b3b9ef7466cd6be918eebe9a58e5e39600f
Laura Carolina Sanchez Cardoso <laura.sanchez.cardoso@fing.edu.uy>
2017-06-09 17:15:51 -0300

    actualizacion ControladorUsuarios h y cpp

--- cpp/ControladorUsuarios.cpp
+++ cpp/ControladorUsuarios.cpp
@@ -1,24 +1,22 @@
 #include "../include/ControladorUsuarios.h"
 
-ControladorUsuarios ControladorUsuarios::ControladorUsuarios(){ 
-    this.instancia = new ControladorUsuarios(); 
-    //this.usuariosDelSistema = ; //como sea que se vaya a inicializar la coleccion     
-    return this.instancia; 
-} 
+ControladorUsuarios ControladorUsuarios::ControladorUsuarios(){
+    this.instancia = new ControladorUsuarios();
+    return this.instancia;
+}
 
-ControladorUsuarios * ControladorUsuarios::getControladorUsuarios(){ 
-    if(this.instancia == NULL) 
-            this.instancia = ControladorUsuarios(); 
-    return this.instancia; 
-}  
+ControladorUsuarios * ControladorUsuarios::getControladorUsuarios(){
+    if(this.instancia == NULL)
+            this.instancia = ControladorUsuarios();
+    return this.instancia;
+}
 
 void ControladorUsuarios::agregarContacto(string cel){
-    this.usuarioIniciado.agregarContacto((this.usuariosDelSistema).find(cel));
-    /*Hacemos tambien el inverso?*/
+    this.usuarioIniciado.agregarContacto(this.usuariosDelSistema[cel]);
 }
 
 Usuario ControladorUsuarios::getUsuario(string cel){
-    return (this.usuarioDelSistema).find(cel);
+    return this.usuariosDelSistema[cel];
 }
 
 Usuario ControladorUsuarios::getUsuarioSesionActual(){
@@ -29,13 +27,24 @@ DataContacto ControladorUsuarios::getDatos(string cel){
     return this.usuarioIniciado.getDataContacto();
 }
 
-void ControladorUsuarios::darseDeAlta(string cel, string urlImagen, string descripcion){
+void ControladorUsuarios::darseDeAlta(string nombre, string cel, string urlImagen, string descripcion){
     Usuario uno = new Usuario();
+    uno.nombre = nombre;
     uno.telefono = cel;
     uno.urlAvatar = urlImagen;
     uno.descripcion = descripcion;
-    //nos comimos el nombre
-    this.usuariosDelSistema.add(uno);
+    this.usuariosDelSistema[cel] = uno;
 }
 
+enumIniciarSesion ControladorUsuarios::iniciarSesion(string cel){
+    return SESION_INICIADA_CORRECTAMENTE;
+}
+
+set<dataContacto> ControladorUsuarios::listarContactos(){
+    return this.usuarioIniciado.getContactos();
+}
+
+void ControladorUsuarios::archivarConversacion(int identificadorConv){
+    this.usuarioIniciado.archivarConversacion(identificadorConv);
+}
 
--- include/ControladorUsuarios.h
+++ include/ControladorUsuarios.h
@@ -1,28 +1,30 @@
 #ifndef CONTROLADORUSUARIOS_H
-#define	CONTROLADORUSUARIOS_H
+#define CONTROLADORUSUARIOS_H
 #include <string>
 using namespace std;
 #include "Usuario.h"
 #include "DataContacto.h"
 #include "enumIniciarSesion.h"
+#include <map>
 
 class ControladorUsuarios {
 private:
-    ControladorUsuarios * instancia = NULL;
+    static ControladorUsuarios * instancia = NULL;
     Usuario * usuarioIniciado = NULL;
+    enumIniciarSesion enumerado;
     ControladorUsuarios();
-    set<Usuario> usuariosDelSistema;
+    map<string,Usuario *> usuariosDelSistema;
     void iniciarSesionUsuario();/*no tengo idea que se supone que haga*/
 public:
-    static ControladorUsuarios getControladorUsuarios();
+    ControladorUsuarios getControladorUsuarios();
     void agregarContacto(string cel);
     Usuario getUsuario(string cel);
     Usuario getUsuarioSesionActual();
     DataContacto getDatos(string cel);
-    void darseDeAlta(string cel, string urlImagen, string descripcion);
+    void darseDeAlta(string nombre, string cel, string urlImagen, string descripcion);
     enumIniciarSesion iniciarSesion(string cel);
     set<dataContacto> listarContactos();
     void archivarConversacion(int identificadorConv);
 };
 
-#endif	/* CONTROLADORUSUARIOS_H */
+#endif  /* CONTROLADORUSUARIOS_H */
