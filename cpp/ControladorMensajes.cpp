a4112a28ea91d5c287c606356b2bc98876da7643
Laura Carolina Sanchez Cardoso <laura.sanchez.cardoso@fing.edu.uy>
2017-06-14 03:02:57 -0300

    Actualizacion ControladorMensajes

--- cpp/ControladorMensajes.cpp
+++ cpp/ControladorMensajes.cpp
@@ -1,10 +1,31 @@
 #include "../include/ControladorMensajes.h"
 
 ControladorMensajes::ControladorMensajes() {
-  this.instancia = new ControladorMensajes();
-  return this.instancia;
+  this->instancia = new ControladorMensajes();
+  return this->instancia;
 }
 
 void ControladorMensajes::enviarMensaje(Mensaje mensaje){
+//ver si pasamos el controlador o el usuario
+}
 
+void ControladorMensajes::seleccionarConversacion(int identificador){
+  this->conversacionSeleccionada = this->conversacionesDelSistema[identificador];
+}
+
+ControladorMensajes ControladorMensajes::getControladorMensajes(){
+  if (this->instancia == NULL)
+    this->instancia = ControladorMensajes();
+
+  return this->instancia;
+}
+
+void ControladorMensajes::crearConversacionSimple(string telefonoContacto){
+//ver si pasamos el controlador o el usuario
+}
+
+set<DataConversacion> ControladorMensajes::darConversaciones(){
+  set<DataConversacion> retorno;
+  //iteracion sobre las conversaciones agregando los DataConversacion a retorno
+  return retorno;
 }
